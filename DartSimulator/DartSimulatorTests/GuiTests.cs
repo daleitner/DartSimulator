635149082970a88d203778e2f3a941cdff084372
Daniel Leitner <dmcleite@hotmail.com>
2017-06-03 12:08:23 +0200

    added TestProject and gui test

---
+++ DartSimulator/DartSimulatorTests/GuiTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ApprovalTests.Reporters;
+using ApprovalTests.Wpf;
+using DartSimulator;
+
+namespace DartSimulatorTests
+{
+	[TestClass]
+	[UseReporter(typeof(TortoiseImageDiffReporter), typeof(ClipboardReporter))]
+	public class GuiTests
+	{
+		[TestMethod]
+		public void CheckDefaultGui()
+		{
+			var window = new MainWindow();
+			WpfApprovals.Verify(window);
+		}
+	}
+}
